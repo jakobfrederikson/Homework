7d3239fb17802cea41e7235335c31c07f7d45316
Jakob <63636667+worzell@users.noreply.github.com>
2021-03-02 16:57:06 +1300

    Added PrimeNumberCheck via upload

---
+++ PrimeNumberCheck/PrimeNumberCheck.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+
+    //cout << "\n \t Enter a number: ";
+    //cin >> a;
+
+    if (a / 2 == 0) {
+        cout << "\n \t " << a << " is not a prime number." << endl;
+    }
+    else {
+        cout << "\n \t " << a << " is a prime number." << endl;
+    }
+}
