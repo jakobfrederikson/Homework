c3a70adfbc5e14e00e0be04b4acf1ee3fb38e5e0
Jakob <63636667+worzell@users.noreply.github.com>
2021-03-16 12:43:25 +1300

    Update PrimeNumberGeneration.cpp
    updating comments

--- PrimeNumberGeneration/PrimeNumberGeneration.cpp
+++ PrimeNumberGeneration/PrimeNumberGeneration.cpp
@@ -8,7 +8,7 @@ int main()
 {
     // Declaring variables 'start' and 'end' to 
     // indicate the range in which the program checks for prime numbers.
-    // Declaring i and j as a counter.
+    // Declaring i and j as a counter in our 'for' loops.
     // primeCheck is used as a boolean (1 or 0) to output a prime number.
     // primeCount is a counter for how many primes have been output.
     int start, end, i, j, primeCheck, primeCount = 0;
@@ -21,8 +21,8 @@ int main()
     cout << "Enter final number: ";
     cin >> end;
 
-    // while loop that won't end until user enters a higher value for the
-    // final number than the starting number.
+    // 'while' loop that checks if the user entered a higher starting value
+    // than their end value. It will continue until the user enters correct input.
     while (start > end) {
         cout << "Please make sure your final value is bigger than the starting number";
         cout << "\n\nEnter starting number: ";
@@ -32,8 +32,8 @@ int main()
     }
 
     cout << "Prime List between (" << start << " and " << end << "):";
-    
-    // for loop to calculate the prime numbers and then output them
+
+    // 'for' loop to calculate the prime numbers and then output them
     for (i = start; i <= end; i++) {
         if (i == 1 || i == 0) {
             continue; // continue here because neither 1 or 0 are 
@@ -41,10 +41,11 @@ int main()
                       // calculation in the below 'for loop'
         }
 
-        primeCheck = 1; // From what I've seen, you must intialize the
-                        // prime check here rather than at the start.
-                        // I'm not sure of the reasoning for this.
+        primeCheck = 1; // Declaring that whatever 'i' is right now
+                        // is a prime number because it didn't equal 1 or 0.
 
+        // starting 'for' loop at 2 because that's the lowest prime number you can have.
+        // this loop checks if 'i' divided by 2 has no remainder.
         for (j = 2; j < i; j++) {
             if (i % j == 0) {
                 primeCheck = 0;
@@ -65,4 +66,4 @@ int main()
 
 // with help from:
 // https://stackoverflow.com/questions/5200879/printing-prime-numbers-from-1-through-100
-// https://w3codeworld.com/article/146/cplusplus-program-to-print-prime-numbers-from-1-to-n-using-for-loop
+// https://w3codeworld.com/article/146/cplusplus-program-to-print-prime-numbers-from-1-to-n-using-for-
